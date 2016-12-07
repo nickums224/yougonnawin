d3c11a9867d5eb0e4b7c299390f57fcd77d70455
Nicholas Umstead (NCUMST3020) <NCUMST3020@ung.edu>
2016-12-07 18:02:51 -0500

    Returns repeated lottery numbers in a set of 6

---
+++ Project.py
@@ -0,0 +1,9 @@
+Python 2.7.8 (default, Jun 30 2014, 16:03:49) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def you_gonna_win():
+"""Returns repeated lottery numbers in a set of 6
+
+>>>you_gonna_win(8, 10, 26, 27, 33)
+"""
+raw
+    return max(set(lst), key=lst.count)
