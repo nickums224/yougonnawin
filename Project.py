455225a8243d494b2b5de99e4257e9ee4d8a6ab6
Nicholas Umstead (NCUMST3020) <NCUMST3020@ung.edu>
2016-12-07 18:53:50 -0500

    okay

--- Project.py
+++ Project.py
@@ -1,10 +1,20 @@
 Python 2.7.8 (default, Jun 30 2014, 16:03:49) [MSC v.1500 32 bit (Intel)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> def you_gonna_win():
-"""Returns repeated lottery numbers in a set of 6
+>>>
+def you_gonna_win(amount=6):
+"""Returns random lottery numbers in list of 6
 
->>>you_gonna_win(8, 10, 26, 27, 33)
+>>>you_gonna_win()
+How many numbers for this game? 6
+[55, 3, 54, 23, 41, 25]
+
 """
-raw
-    return max(set(lst), key=lst.count)
-hgskfjdhkdf
+
+from random import randint as numb
+    return [numb(1,69) for number in range(amount)]
+
+def tell_me_what(prompt="how many lines would you like? "):
+    return int(input(prompt))
+
+
+
