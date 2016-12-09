a2d2e6466c33593d109151c482416652b9610f2e
nickums224 <ncumst3020@ung.edu>
2016-12-09 09:52:29 -0500

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,15 +1,22 @@
-import random
-def you_gonna_win():
+
+  import random
+
+def you_gonna_win(amount=6):
 """Returns random lottery numbers in list of 6
 
 >>> you_gonna_win(1)
 [55, 3, 54, 23, 41, 25]
 
+>>> you_gonna_win(3)
+[60, 5, 15, 18, 23, 4]
+[7, 44, 67, 3, 34, 31]
+[17, 8, 4, 6, 61, 45]
+
 """
-amount = int(raw_input("How many times are you going to play?"))
-return[(numb(1,69) for i in range(amount)]
+radint = numb
+    return [numb(1,69) for i in range(amount)]
 
-
-        
+def get_user_input(prompt="How many times are you going to play?"):
+    return int(input(prompt))      
 
 
